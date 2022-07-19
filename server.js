e05867c65ac5edd00394a6276733660e7e929b00
Mobasshir <mobasshirhasan20@gmail.com>
2022-07-19 22:17:24 +0530

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
 const postinfo= require('./model')
-const multer=require('multer')
 const app=express();
 const cors= require("cors")
 mongoose.connect("mongodb+srv://mobasshir:atlas1234@cluster0.tw3by.mongodb.net/instaclone?retryWrites=true&w=majority")
 app.use(express.json())
-app.use(multer)
 app.use(cors())
 app.use(express.urlencoded({extended:false}))
-app.listen(process.env.PORT||3001,(err)=>{
+app.listen(process.env.PORT,5000,(err)=>{
     if(err){
         console.log(err)
     }
@@ -18,22 +15,11 @@ app.listen(process.env.PORT||3001,(err)=>{
         console.log("server is running")
     }
 })
-
 app.get("/", (req,res)=>{
-    res.send("instaclone backend")
+    res.send("InstaClone Backend")
 })
-app.get("/posts", async (req,res)=>{
-    try{
-        const data = await postinfo.find({})
-        res.status(200).send(data)
-    }
-    catch{
-        res.status(400).send("an error occured while getting posts")
-    }
-} ) 
 app.post("/post",async (req,res)=>{
     try{
-
         const {image,author,location,description}=req.body
         const register =  new postinfo({
             image,author,location,description,Date:new Date().toJSON().slice(0, 10),likes:Math.floor(Math.random()*200)
@@ -47,3 +33,12 @@ app.post("/post",async (req,res)=>{
         res.status(400).send("an error occured while posting")
     }
 })
+app.get("/posts", async (req,res)=>{
+    try{
+        const data = await postinfo.find({})
+        res.status(200).send(data)
+    }
+    catch{
+        res.status(400).send("an error occured while getting posts")
+    }
+} ) 
